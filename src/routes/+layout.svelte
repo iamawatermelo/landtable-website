8dd09950ec2a72abfc8caa362255fe8286acdd6f
Sarah <>
2024-12-31 19:58:44 +0000

    more stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Landtable from '$lib/icons/Landtable.svg?component';
   import type { Snippet } from 'svelte';
 
   let {
@@ -11,4 +12,65 @@
   import '$lib/utility.scss';
 </script>
 
+<nav>
+  <ul>
+    <li><a href="/" class="icon"><Landtable /><span>landtable</span></a></li>
+    <div>
+      <li><a href="/">Platform</a></li>
+      <li><a href="/">Proxy</a></li>
+      <li><a href="/">Docs</a></li>
+      <li><a href="/">Hack Club log in</a></li>
+    </div>
+  </ul>
+</nav>
+
 {@render children?.()}
+
+<style lang="scss">
+  nav {
+    position: sticky;
+    height: 4em;
+    border-bottom: 1px solid rgb(var(--fg-color), 0.2);
+    padding: 1em;
+    
+    ul {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      height: 100%;
+      max-width: 100em;
+      margin: auto;
+      padding: unset;
+      list-style: none;
+      
+      li {
+        margin: unset;
+        padding: unset;
+      }
+      
+      div {
+        display: flex;
+        flex-direction: row;
+        gap: 1em;
+      }
+      
+      .icon {
+        display: flex;
+        align-items: center;
+        font-size: 1.5em;
+        gap: .25em;
+        text-decoration: none;
+        
+        :global(svg) {
+          height: 1em;
+          width: 1em;
+        }
+        
+        span {
+          display: block;
+        }
+      }
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,154 +6,180 @@
   import Layer4 from '$lib/assets/herolayers/layer4.svg?component';
 </script>
 
-<div class="hero">
-  <div class="hero-left">
-    <h1>Take back control<br /> of your data.</h1>
-    <p class="hero-text">
-      Migrate, update, visualise and automate your data with the Landtable Platform.
-      Effortlessly.
-    </p>
-    
-    <a href="https://github.com/iamawatermelo/landtable" class="hero-text">
-      Keep up with development on GitHub <span class="arrow"><Arrow /></span>
-    </a>
-    
-    <p>And, for Hack Clubbers, get a hosted Landtable instance for free. Launches mid-Feburary.</p>
-  </div>
-  <div class="hero-right" role="none">
-    <div class="stack">
-      <Layer1 />
-      <Layer2 />
-      <Layer3 />
-      <Layer4 />
+<div class="hero-outer">
+  <div class="hero">
+    <div class="hero-left">
+      <h1>Take back control<br /> of your data.</h1>
+      <p class="hero-text">
+        Migrate, update, visualise and automate your data with the Landtable Platform.
+        Effortlessly.
+      </p>
+      
+      <a href="https://github.com/iamawatermelo/landtable" class="hero-text">
+        Keep up with development on GitHub<span class="arrow"><Arrow /></span>
+      </a>
+      
+      <p>And, for Hack Clubbers, get a hosted Landtable instance for free. Launches mid-Feburary.</p>
+    </div>
+    <div class="hero-right" role="none">
+      <div class="stack">
+        <Layer1 />
+        <Layer2 />
+        <Layer3 />
+        <Layer4 />
+      </div>
     </div>
   </div>
 </div>
 
+<p>florf</p>
+
 <style lang="scss">
-  .hero {
+  .hero-outer {
     display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    padding: 2em;
-    background:
-      url('$lib/assets/noise.svg'),
-      linear-gradient(-35deg, #F8EEFF 0%, #BBBEFF 100%);
-    background-size: cover;
-    border: 1em solid white;
-    overflow: clip;
-    gap: 4em;
-    
-    @media screen and (min-width: 32em) {
-      border-width: 2em;
-    }
-    
-    @media screen and (min-width: 64em) {
-      font-size: 1.2em;
-      padding-left: 4em;
-      flex-direction: row;
-    }
-    
-    @media screen and (min-width: 128em) {
-      font-size: 1.5em;
-    }
-    
-    .hero-left {
+    justify-content: center;
+    min-height: 70vh;
+  
+    .hero {
+      margin: 4em;
+      top: 0;
+      bottom: 0;
+      
       display: flex;
-      flex: 1 1 auto;
       flex-direction: column;
-      justify-content: center;
-      max-width: min-content;
-      gap: 2em;
+      width: calc(100% - 2em);
+      max-width: 100rem;
+      margin: 2em auto;
+      height: max-content;
+      padding: 2em;
+      background:
+        url('$lib/assets/noise.svg'),
+        linear-gradient(-35deg, #F8EEFF 0%, #BBBEFF 100%);
+      background-size: cover;
+      overflow: clip;
+      gap: 4em;
+      border-radius: 1em;
       
-      br {
-        display: none;
+      @media (prefers-color-scheme: dark) {
+        color: #EAEBFF;
         
-        @media screen and (min-width: 48em) {
-          display: block;
-        }
-      }
-      
-      h1 {
-        font-size: 2em;
-        letter-spacing: .05rem;
-        width: max-content;
-        max-width: 100%;
-      }
-      
-      .hero-text {
-        font-size: 1.2em;
+        background:
+          url('$lib/assets/noise.svg'),
+          linear-gradient(-35deg, #2D317F 0%, #6B2D7F 100%);
       }
       
       @media screen and (min-width: 32em) {
+        border-width: 2em;
+      }
+      
+      @media screen and (min-width: 72em) {
+        padding: 4em;
+        flex-direction: row;
+        font-size: 1.2em;
+      }
+      
+      @media screen and (min-width: 128em) {
+        font-size: 1.5em;
+      }
+      
+      .hero-left {
+        display: flex;
+        flex: 1 1 auto;
+        flex-direction: column;
+        justify-content: center;
+        max-width: min-content;
+        gap: 2em;
+        
+        br {
+          display: none;
+          
+          @media screen and (min-width: 48em) {
+            display: block;
+          }
+        }
+        
         h1 {
-          font-size: 4em;
+          font-size: 2em;
+          // letter-spacing: .05rem;
+          width: max-content;
+          max-width: 100%;
         }
         
         .hero-text {
-          font-size: 1.5em;
+          font-size: 1.2em;
+        }
+        
+        @media screen and (min-width: 32em) {
+          h1 {
+            font-size: 4em;
+          }
+          
+          .hero-text {
+            font-size: 1.5em;
+          }
         }
       }
-    }
-    
-    .hero-right {
-      flex: 1 1 auto;
-      width: 100%;
-      position: relative;
-      height: 16em;
       
-      @media screen and (min-width: 48em) {
-        height: 32em;
-      }
-      
-      @media screen and (min-width: 100em) {
-        left: 4em;
-        height: unset;
-      }
-      
-      .stack {
-        position: absolute;
-        top: 50%;
-        left: 2em;
-        --scale-factor: 0.7;
-        
-        transform: rotate3d(0.7, 0.5, -0.3, 0.1turn) scale(var(--scale-factor)) translateY(calc(-50% + 128px));
-        transform-style: preserve-3d;
+      .hero-right {
+        flex: 1 1 auto;
+        width: 100%;
+        position: relative;
+        height: 16em;
         
         @media screen and (min-width: 48em) {
-          --scale-factor: 1.2;
+          height: 32em;
+          --scale-factor: 1.5;
         }
         
-        @media screen and (min-width: 100em) {
-          left: 0;
+        @media screen and (min-width: 72em) {
+          left: 4em;
+          height: unset;
         }
         
-        @media screen and (min-width: 128em) {
-          --scale-factor: 2;
-        }
-        
-        :global(svg) {
+        .stack {
           position: absolute;
-          border-radius: 16px;
+          top: 50%;
+          left: 2em;
+          --scale-factor: 0.7;
           
-          &:nth-child(1) {
-            transform: translateZ(2em) translateY(-50%);
-            box-shadow: -8px 8px 16px 0px rgba(0,0,0,0.2);
+          transform: rotate3d(0.7, 0.5, -0.3, 0.1turn) scale(var(--scale-factor)) translateY(calc(-50% + 128px));
+          transform-style: preserve-3d;
+          
+          @media screen and (min-width: 48em) {
+            --scale-factor: 1.5;
           }
           
-          &:nth-child(2) {
-            transform: translateZ(4em) translateY(-50%);
-            filter: drop-shadow(-8px 8px 16px rgba(0,0,0,0.1));
+          @media screen and (min-width: 100em) {
+            left: 0;
           }
           
-          &:nth-child(3) {
-            transform: translateZ(6em) translateY(-50%);
-            filter: drop-shadow(-8px 8px 16px rgba(0,0,0,0.1));
+          @media screen and (min-width: 128em) {
+            --scale-factor: 2;
           }
           
-          &:nth-child(4) {
-            transform: translateZ(8em) translateY(-50%);
-            filter: drop-shadow(-8px 8px 16px rgba(0,0,0,0.1));
+          :global(svg) {
+            position: absolute;
+            border-radius: 16px;
+            
+            &:nth-child(1) {
+              transform: translateZ(2em) translateY(-50%);
+              box-shadow: -8px 8px 16px 0px rgba(0,0,0,0.2);
+            }
+            
+            &:nth-child(2) {
+              transform: translateZ(4em) translateY(-50%);
+              filter: drop-shadow(-8px 8px 16px rgba(0,0,0,0.1));
+            }
+            
+            &:nth-child(3) {
+              transform: translateZ(6em) translateY(-50%);
+              filter: drop-shadow(-8px 8px 16px rgba(0,0,0,0.1));
+            }
+            
+            &:nth-child(4) {
+              transform: translateZ(8em) translateY(-50%);
+              filter: drop-shadow(-8px 8px 16px rgba(0,0,0,0.1));
+            }
           }
         }
       }
